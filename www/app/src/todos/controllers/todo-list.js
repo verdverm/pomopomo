c8c577847730886ebd283eafe148cf48217eab1a
tony <verdverm@gmail.com>
2015-09-05 23:39:46 -0400

    fixing a timing/sequence issue with auth not finishing by todo list request

--- www/app/src/todos/controllers/todo-list.js
+++ www/app/src/todos/controllers/todo-list.js
@@ -1,23 +1,32 @@
 angular.module("pomodoroTodoApp")
-    
-.controller("TodoListController", 
-    function($rootScope, $scope, $location, $mdDialog, $mdToast, todoService) {
+
+.controller("TodoListController",
+    function($rootScope, $scope, $location, $mdDialog, $mdToast, authService, todoService) {
 
 		var self = this;
 		self.todos = [];
 
 		self.newTodo = newTodo;
 
-		
+		// load user's todo, but not until we are authed
+		// we need this because a user can directly navigate to this page
+	    $scope.auth = authService
+	    $scope.$watch('auth.authed()', function(newVal) {
+	        console.log("authed update: ", newVal)
 
-		var promise = todoService.getAllTodos();
+	        if (newVal === false) {
+	        	return
+	        }
+			var promise = todoService.getAllTodos();
 
-		promise.then(function(todos) {
-			self.todos = todos;
-			console.log("todos received: ", todos)
-		}, function(reason) {
-			alert('Retrieving todos failed: ' + reason);
-		});
+			promise.then(function(todos) {
+				self.todos = todos;
+				console.log("todos received: ", todos)
+			}, function(reason) {
+				alert('Retrieving todos failed: ' + reason);
+			});
+	    })
+
 
 
 
@@ -31,7 +40,7 @@ angular.module("pomodoroTodoApp")
 		    })
 		    .then(function(todo) {
 		      console.log("new todo: ", todo)
-		      
+
 		      if ( todo.Name === "" ) {
                 $mdToast.show($mdToast.simple().content("unable to create todo :[\n" + "empty name"));
 		      	return;
@@ -54,7 +63,7 @@ angular.module("pomodoroTodoApp")
 		}
 
 		function DialogController($scope, $mdDialog) {
-		  
+
 		  $scope.todo = {
 		  	Name: "",
 		  	Description: ""
